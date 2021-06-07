b2034744009d25edc5cd6849263f3676a7c093ec
imthevk <krvijay878@gmail.com>
2021-06-07 22:05:56 +0530

    hex color generator in vanilla js completed.

---
+++ color-flipper/index.js
@@ -0,0 +1,20 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.querySelector(".btn");
+// console.log(btn);
+const color = document.querySelector(".color");
+// console.log(color);
+btn.addEventListener("click", function () {
+  let hexcolor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexcolor += hex[getRandomNumber()];
+  }
+  //   console.log(hexcolor);
+  color.textContent = hexcolor;
+
+  document.body.style.backgroundColor = hexcolor;
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
